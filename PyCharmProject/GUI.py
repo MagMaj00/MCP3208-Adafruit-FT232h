287b34c84e930f384ea8d4c5fe4265d8bb0294bc
Magdalena Majcher <mgdmjr@gmail.com>
2023-06-26 17:30:56 +0200

    remove dead code

--- PyCharmProject/GUI.py
+++ PyCharmProject/GUI.py
@@ -1,11 +1,9 @@
 import tkinter as tk
 import board
 import digitalio
-import time
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import threading
+
 
 # Adafruit ft323H configuration
 
